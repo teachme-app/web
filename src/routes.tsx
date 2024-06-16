d4b4e5c7231363ed90949f97335d05d8c1bd4b02
Maria <mariaisabelacastanho@gmail.com>
2024-06-15 23:48:49 -0300

    Menu + DescCursos

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { DescCursos } from './pages/CursoDesc'
 import { Carrinho } from './pages/Carrinho'
+import { Navegar } from './pages/Navegar'
 
 export const appRoutes = createBrowserRouter([
   {
@@ -15,5 +16,9 @@ export const appRoutes = createBrowserRouter([
   {
     path: '/carrinho',
     element: <Carrinho />,
+  },
+  {
+    path: '/Navegar',
+    element: <Navegar />,
   },
 ])
