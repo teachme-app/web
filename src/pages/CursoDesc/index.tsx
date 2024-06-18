4de64ede55112be09d65bc3dbbc82c084f23af77
Maria Isabela <mariaisabelacastanho@gmail.com>
2024-06-18 12:05:41 -0300

    Desc Arrumados

--- src/pages/CursoDesc/index.tsx
+++ src/pages/CursoDesc/index.tsx
@@ -5,7 +5,7 @@ export const DescCursos = () => {
   return (
     <>
       <MenuNav />
-      <S.Container>
+      {/* <S.Container>
         <S.LeftSideContainer>
           <div>
             <S.img src='/place-holder-image.png' alt='place holder'  />
@@ -42,7 +42,7 @@ export const DescCursos = () => {
               </p>
             </div>
           </S.div2>
-          </S.RightSideContainer>
+          
           <S.BottomSideContainer id='container-bottom'>
             <S.div3>
               <div>
@@ -51,8 +51,66 @@ export const DescCursos = () => {
               </div>
             </S.div3>
           </S.BottomSideContainer>
-        
-      </S.Container>
+          </S.RightSideContainer>
+      </S.Container> */}
+      <S.Container>
+        <S.LeftSideContainer>
+
+          <S.ImageContainer>
+          <div>
+            <S.img src='/place-holder-image.png' alt='place holder'  />
+          </div>
+          </S.ImageContainer>
+
+          <S.TitleContainer>
+
+            <h1>Título do curso</h1>
+            <p></p>
+          </S.TitleContainer>
+
+          <S.TextContainer>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis ullamcorper elementum.
+              Donec molestie lacinia purus, ac dapibus velit sollicitudin maximus. Donec at vulputate felis.
+              Etiam egestas luctus ante, eget pulvinar felis. Etiam ut maximus metus. Maecenas sit amet erat
+              libero. Aliquam erat volutpat. Nulla vulputate cursus nulla eget sollicitudin. Suspendisse
+              potenti. Nam blandit eleifend gravida. Maecenas eget semper dui. Ut in justo porttitor,
+              pellentesque lacus et, gravida risus.
+            </p>
+          </S.TextContainer>
+
+          <S.HeartButton>
+            <img src='/coracao.svg' alt='coracao' width='82' height='81' />
+          </S.HeartButton>
+
+        </S.LeftSideContainer>
+
+
+        <S.RightSideContainer id='container-direita'>
+          <S.div2>
+            <div>
+              <S.img2 src='/PessoaBolinha.png' alt='pessoa'  />
+              <h1>Perfil do professor</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis ullamcorper elementum.
+                Donec molestie lacinia purus, ac dapibus velit sollicitudin maximus. Donec at vulputate felis.
+                Etiam egestas luctus ante, eget pulvinar felis. Etiam ut maximus metus. Maecenas sit amet erat
+                libero.{' '}
+              </p>
+            </div>
+          </S.div2>
+          
+          <S.BottomSideContainer id='container-bottom'>
+            <S.div3>
+              <div>
+                <h1>R$XXXXXXX,XXXXXX</h1>
+                <S.button> Adicionar ao Carrinho </S.button>
+              </div>
+            </S.div3>
+          </S.BottomSideContainer>
+          </S.RightSideContainer>
+      </S.Container> 
+
     </>
   )
 }
--- src/pages/CursoDesc/styles.ts
+++ src/pages/CursoDesc/styles.ts
@@ -2,7 +2,7 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
 
   body {
@@ -11,25 +11,26 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 
-export const Container = styled.div`
-backgroundColor: '#99999',
+export const ImageContainer = styled.div`
+img{
+width: 98%;
+margin-left: 4%;
+}
 
 `
 
 export const LeftSideContainer = styled.div`
 width: 50%;
-background-color: ;
+
 height: 100vh;
 display: flex;
 flex-direction: column;
 `
 
 export const img = styled.img`
-
-margin-top: 30px;
-margin-left: 40px;
+margin-top: 6%;
+  margin-left: 40px;
   display: flex;
-  width: 100%;
 
 
 `
@@ -38,7 +39,7 @@ margin-left: 40px;
 export const TitleContainer = styled.div`
     h1 {
       
-        font-size: 60px;
+        font-size:50px;
         text-decoration: underline;
         color: #FF9740;
     }
@@ -54,8 +55,8 @@ color: #545454;
 font-weight: bold;
 `
 export const HeartButton = styled.button`
-margin-left: 94%;
-margin-top: -37%;
+margin-left: 91%;
+margin-top: -45%;
 height: 30%;
 background-color: #00000000;
 border:none
@@ -63,27 +64,34 @@ border:none
 
 `
 export const RightSideContainer = styled.div`
-width: 44%;
-  height: 490px;
-  margin-left: 55%;
-  margin-top: -52%;
-  background-color: #383141;
-  border-radius: 52px;
+width: 50%;
+  height: 100vh;
+  margin-left: 1%;
+  margin-top: 2%;
+
+  border-radius: 0px;
 
 
 
 `
 export const div2 = styled.div`
+background-color: #383141;
+border-radius: 40px;
+width: 90%;
+height: 60%;
+margin-left: auto;
+margin-right: auto;
+margin-top: 2%;
 h1{
-margin-left: 45%;
-font-size: 60px;
-margin-top: -25%;
+margin-left: 48%;
+font-size: 370%;
+margin-top: -33%;
 color: #FF9740;
 }
 
 p { 
 color: #FBF4EA;
-margin-top: 11%;
+margin-top: 13%;
 margin-left: 5%;
 margin-right: 1%;
 font-size: 99%;
@@ -92,24 +100,25 @@ font-weight: 600;
 }
 `
 export const BottomSideContainer = styled.div`
-margin-left: 62%;
+margin-left:16%;
 margin-top: 3%;
 
 `
 export const div3 = styled.div`
 h1{
-font-size: 370%;
+font-size: 50px;
 color: #FF9740;
 }
 `
 export const button = styled.button`
-margin-left: 10%;
+margin-left: -5px;
 margin-top: 5%;
+
 font-size: 30px;
 font-weight: 600;
 color: #383141;
-height: 100px;
-width: 450px;
+height: 10vh;
+width: 82%;
 
 background-color: #D9D9D9;
 border: none;
@@ -120,10 +129,15 @@ border-radius: 45px;
   }
 `
 export const img2 = styled.img`
-width: 290px;
-height: 290px;
-margin-top: 5%;
+width: 40%;
+height: 40%;
+margin-top: 4%;
 margin-left: 5%;
-
 `
-export default GlobalStyle;
+
+
+export const Container = styled.div`
+  display: flex;
+  widht: 100vh;
+  height: 100vh;
+`
