37090f43d51717416f197a20996b6235862d6f78
Gustavo Maia <gustavopmaia@hotmail.com>
2024-10-29 21:34:26 -0300

    prod

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const ENV = process.env.NODE_ENV || 'PROD' // Use environment variable or default to 'PROD'
-
 export const apiInstance = axios.create({
   baseURL: 'https://teachme-app.com.br/api/v1',
   headers: {
